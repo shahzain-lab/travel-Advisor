1ff7774bf63501d0adcfa4b9469853b0fa292e40
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-04 04:04:59 -0500

    travel advisor app initialized files str

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Header,
+    List,
+    Map,
+    PlaceDetails
+} from './components'
+
+function App() {
+    return (
+        <div>
+            Hello Worlds from vscode
+        </div>
+    )
+}
+
+export default App
