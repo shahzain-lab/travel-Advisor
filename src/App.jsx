6f1bb7e1a02bf367ef69b141a4ba9e0c15ee54c4
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-04 05:44:18 -0500

    added material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { CssBaseline,Grid } from '@mui/material';
 import React from 'react';
 import {
     Header,
     List,
     Map,
-    PlaceDetails
 } from './components'
 
 function App() {
     return (
-        <div>
-            Hello Worlds from vscode
-        </div>
+        <>
+        <CssBaseline />
+         <Header />
+         <Grid container spacing={3} style={{width: '100%'}}>
+             <Grid item xs={12} md={4}>
+                 <List />
+             </Grid>
+             <Grid item xs={12} md={8}>
+                 <Map />
+             </Grid>
+         </Grid>
+        </>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { SearchOutlined } from '@mui/icons-material'
+import { AppBar, InputBase, Toolbar, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { Autocomplete } from '@react-google-maps/api'
+import React from 'react';
+import useStyle from './style'
 
 function Header() {
+    const classes = useStyle()
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new Places
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchOutlined />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
