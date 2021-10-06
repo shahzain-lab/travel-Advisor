cb400fa03d050552031647244abace2f7fb96a4e
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-06 13:31:10 -0500

    accessed google map api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react'
+import React from 'react';
+import useStyles from './style'
 
 function Map() {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('min-width: 600px')
+    const coordinate = { let: 0, lng: 0 };
+
     return (
-        <div>
-            <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+             key= 'AIzaSyDbYqGyO2jC_zrLLr2EeYTNxlC9Sd9BEpM'
+             defaultCenter={coordinate}
+             center={coordinate}
+             defaultZoom={14}
+             margin={[50, 50, 50, 50]}
+            //  option= {''}
+            //  onChange={''}
+            //  onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
